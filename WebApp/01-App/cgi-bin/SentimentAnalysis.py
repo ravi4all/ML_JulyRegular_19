5ac2b3941021aa846d8f4ed5b25d939e2543a2b8
ravi4all <ravikanttyagi.2295@gmail.com>
2019-08-08 15:29:33 +0530

    Add files via upload

--- WebApp/01-App/cgi-bin/SentimentAnalysis.py
+++ WebApp/01-App/cgi-bin/SentimentAnalysis.py
@@ -3,21 +3,7 @@ import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.linear_model import LogisticRegression
-
-df_1 = pd.read_csv('imdb_labelled.txt',sep='\t',header=None)
-df_2 = pd.read_csv('amazon_cells_labelled.txt',sep='\t',header=None)
-df_3 = pd.read_csv('yelp_labelled.txt',sep='\t',header=None)
-
-df = pd.DataFrame()
-df = df.append(df_1)
-df = df.append(df_2)
-df = df.append(df_3)
-
-df.columns = ['Review','Sentiment']
+import pickle as pkl
 
 def textProcessing(document):
     tokens = []
@@ -41,20 +27,15 @@ def textProcessing(document):
     
     return wordsList
 
-def train():
-
-    wordsList = textProcessing(df)
-    cv = CountVectorizer()
-    vect = cv.fit_transform(wordsList)
-    y = df['Sentiment']
-    x_train,x_test,y_train,y_test = train_test_split(vect,y,test_size=0.25)
-
-    reg = LogisticRegression()
-    reg.fit(x_train, y_train)
-    return reg, cv
-
 def test(rev):
-    reg,cv = train()
+    file = open('model.pkl','rb')
+    reg = pkl.load(file)
+    file.close()
+
+    file = open('cv.pkl','rb')
+    cv = pkl.load(file)
+    file.close()
+
     test_df = pd.DataFrame({"Review":[rev]})
     wordsList = textProcessing(test_df)
     v = cv.transform(wordsList)
